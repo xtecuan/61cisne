01670ca8652490e343288636ee3d0730ac4c4e3a
jaimerojascastro@gmail.com <jaimerojascastro@gmail.com@ac51addf-da2e-bf10-1b3b-3d34b3c06471>
2013-03-13 20:14:53 +0000

    nuevos cambios

--- trunk/JavaExamples/src/sv/edu/ufg/ExamplesClass/lectorArgs.java
+++ trunk/JavaExamples/src/sv/edu/ufg/ExamplesClass/lectorArgs.java
@@ -4,16 +4,25 @@
  */
 package sv.edu.ufg.ExamplesClass;
 
+import java.io.FileWriter;
+
 /**
  *
  * @author jaimerojas
  */
-public class lectorArgs {
-    public static void main(String[] args) {
+public class lectorArgs  {
+    public static void main(String[] args)throws Exception  {
+         FileWriter fw = new FileWriter("c:/file.dat");
         if (args.length>0)
         {
-            System.out.println("entre");
+            System.out.println("Hay "+args.length+" argumentos"); 
+            int i; 
+            for (i=0;i<args.length;i++) 
+            { 
+              System.out.println("Parámetro"+i+":"+args[i]); 
+                      fw.write(args[i] + "\n");
+            }
         }
+      fw.close();                                      
     }
-    
 }
