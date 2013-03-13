b7401a2bb9ea0ed2fea99ae57e081b8ade918f50
jaimerojascastro@gmail.com <jaimerojascastro@gmail.com@ac51addf-da2e-bf10-1b3b-3d34b3c06471>
2013-03-13 19:37:45 +0000

    Código de ejemplos para clases de JAVA CICLO I 2013

---
+++ JavaExamples/src/sv/edu/ufg/ExamplesClass/lectorArgs.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.edu.ufg.ExamplesClass;
+
+/**
+ *
+ * @author jaimerojas
+ */
+public class lectorArgs {
+    public static void main(String[] args) {
+        if (args.length>0)
+        {
+            System.out.println("entre");
+        }
+    }
+    
+}
