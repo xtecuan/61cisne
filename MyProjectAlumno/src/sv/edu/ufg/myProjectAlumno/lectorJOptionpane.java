e183532c2a4c8aeafe0bf8dec062fcffb9ba8d5a
jaimerojascastro@gmail.com <jaimerojascastro@gmail.com@ac51addf-da2e-bf10-1b3b-3d34b3c06471>
2013-03-15 06:42:17 +0000

    My proyecto de lectura de array e impresion

---
+++ MyProjectAlumno/src/sv/edu/ufg/myProjectAlumno/lectorJOptionpane.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.edu.ufg.myProjectAlumno;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.swing.JOptionPane;
+import sv.edu.ufg.ejemplos.obtenerentrada.teclado.Alumno;
+import java.io.*;
+
+/**
+ *
+ * @author jaimerojas
+ */
+public class lectorJOptionpane {
+      Alumno alumno = new Alumno();
+      Alumno arrayAlumno[];
+      /*int noAlumnosint = 0;
+      int i=0;*/
+      int otros = 0;
+      
+      int noAlumnosint = Integer.parseInt(JOptionPane.showInputDialog("No de Alumnos a Registrar"));
+
+        
+ 
+}
