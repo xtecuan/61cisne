d6bfee9916c74b3fc620ce84231fd932bc8a66dd
jaimerojascastro@gmail.com <jaimerojascastro@gmail.com@ac51addf-da2e-bf10-1b3b-3d34b3c06471>
2013-03-21 03:03:33 +0000

    Formitas geometricas me falta clase de la cuadratica

---
+++ FormasGeometricas/src/sv/edu/ufg/formasgeometricas/Rectangulo.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.edu.ufg.formasgeometricas;
+
+/**
+ *
+ * @author javaee
+ */
+public class Rectangulo {
+    private double base;
+    private double altura;
+
+    public Rectangulo() {
+    }
+
+    public Rectangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangulo{" + "base=" + base + ", altura=" + altura + '}';
+    }
+            
+  public double calcularArea (){
+      return base*altura;
+  }
+}
