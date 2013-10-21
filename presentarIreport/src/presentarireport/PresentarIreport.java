1f6cc4f0dc7cc460fa3e6b7d857449b3fa2fff01
jaimerojascastro@gmail.com <jaimerojascastro@gmail.com@ac51addf-da2e-bf10-1b3b-3d34b3c06471>
2013-10-21 17:29:23 +0000

    Proyecto para iReport

---
+++ presentarIreport/src/presentarireport/PresentarIreport.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package presentarireport;
+
+import Reporte.presentarReporte;
+
+/**
+ *
+ * @author jaimerojas
+ */
+public class PresentarIreport {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      //  new presentarReporte.setVisble(true);
+    }
+}
